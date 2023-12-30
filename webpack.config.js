30a8d995d85635cb049308b1ec6854126aa6a9e7
Flobtmkg <florian.couturier33@gmail.com>
2023-12-31 00:53:55 +0100

    Add a config switch to make expo export:web and npm run web work with two differents configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
+
+  console.log(".............");
+  console.log("CONFIG MIDDLEWARE....\n.............");
+  console.log("NPM SCRIPT : " + JSON.stringify(process.env.npm_lifecycle_script));
   
-  // Force relative path to index.html for export images, css, js
-  process.env.WEB_PUBLIC_URL="./";
-  // if mode != production WEB_PUBLIC_URL will be replace by "/" which is annoying
-  env.mode="production";
-  env.pwa=false;
 
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  // middleware config can be set here before return
-  // console.log(JSON.stringify(config));
+  if(process.env.npm_lifecycle_script === "expo"){
+    console.log("Config modding required for export");
+    // Force relative path to index.html for export images, css, js
+    process.env.WEB_PUBLIC_URL="./";
+    // if mode != production WEB_PUBLIC_URL will be replace by "/" which is annoying
+    env.mode="production";
+    env.pwa=false;
 
-  config.plugins.forEach(plugin => {
-    if(plugin.userOptions !== undefined && plugin.options !== undefined){
-      // that's probably the plugin we're lookin' for
-      // let's change the default template by our modified one
-      console.log("plugin found! let's force the template source...");
-      // the customized template adds a reference to a env.js that will be added later
-      // by a .sh script referenced in the dockerfile and will transmit system ENV to js.
-      // This way, we can configure somme front-end js variables by system environnement variables controlled by Docker config. 
-      plugin.userOptions.template="./src/index.html";
-      plugin.options.template="./src/index.html";
-    }
-  });
+    const config = await createExpoWebpackConfigAsync(env, argv);
+    // middleware config can be set here before return
+    // console.log(JSON.stringify(config));
+    console.log(config.plugins);
+    config.plugins.forEach(plugin => {
+      if(typeof plugin.userOptions !== 'undefined' && plugin.userOptions.template !== null && plugin.userOptions.template !== ""){
+        // that's probably the plugin we're lookin' for
+        // let's change the default template by our modified one
+        console.log("plugin found! let's force the template source...");
+        // the customized template adds a reference to a env.js that will be added later
+        // by a .sh script referenced in the dockerfile and will transmit system ENV to js.
+        // This way, we can configure somme front-end js variables by system environnement variables controlled by Docker config. 
+        plugin.userOptions.template="./src/index.html";
+      }
+    });
+    console.log(".............\n");
+    return config;
+  }
 
-  return config;
+  else {
+    console.log("Default config, no config modding required");
+    console.log(".............\n");
+    return await createExpoWebpackConfigAsync(env, argv);
+  }
+
 };
