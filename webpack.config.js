73a955d1da6952cc8f56f437d231479e0d745c28
Flobtmkg <florian.couturier33@gmail.com>
2023-12-31 03:33:04 +0100

    Improvement of webpack middleware logic to bundle for production or test on development server

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ module.exports = async function (env, argv) {
   console.log(".............");
   console.log("CONFIG MIDDLEWARE....\n.............");
   console.log("NPM SCRIPT : " + JSON.stringify(process.env.npm_lifecycle_script));
-  
 
-  if(process.env.npm_lifecycle_script === "expo"){
-    console.log("Config modding required for export");
+  if(process.env.BABEL_ENV === "production"){
+    console.log("Config modding required for production export");
     // Force relative path to index.html for export images, css, js
     process.env.WEB_PUBLIC_URL="./";
     // if mode != production WEB_PUBLIC_URL will be replace by "/" which is annoying
@@ -18,16 +17,19 @@ module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(env, argv);
     // middleware config can be set here before return
     // console.log(JSON.stringify(config));
-    console.log(config.plugins);
+
     config.plugins.forEach(plugin => {
-      if(typeof plugin.userOptions !== 'undefined' && plugin.userOptions.template !== null && plugin.userOptions.template !== ""){
-        // that's probably the plugin we're lookin' for
+      // HtmlWebpackPlugin
+      if(plugin.constructor.name === "HtmlWebpackPlugin"){
         // let's change the default template by our modified one
         console.log("plugin found! let's force the template source...");
         // the customized template adds a reference to a env.js that will be added later
         // by a .sh script referenced in the dockerfile and will transmit system ENV to js.
         // This way, we can configure somme front-end js variables by system environnement variables controlled by Docker config. 
         plugin.userOptions.template="./src/index.html";
+        if(typeof plugin.options !== 'undefined'){
+          plugin.options.template = "./src/index.html";
+        };
       }
     });
     console.log(".............\n");
